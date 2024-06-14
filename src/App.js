91e0cf8cdfb7786304a18157061e0121d2250533
KKKKKIKKKK <cupit2030@gmail.com>
2024-06-14 11:25:57 +0900

    리액트장-shop

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import {Button,Navbar,Container,Nav, useAccordionButton} from 'react-bootstrap';
+import 배경이미지 from './img/bg.png'
+import { useState } from 'react';
+
 
 function App() {
+
+  let [shoes] =useState()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      {/* <Button variant="primary">Primary</Button> */}
+
+      <Navbar bg="light" data-bs-theme="light">
+        <Container>
+          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar> 
+
+      <div className="main-bg"></div>
+      {/* 상품 레이아웃3개 만들기 */}
+      
+      <div className="container">
+      <div className="row">
+        <div className="col-md-4">
+        <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%"></img>
+        <h4>상품명</h4>
+        <p>상품설명</p>
+        </div>
+        
+        <div className="col-md-4">
+        <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%"></img>
+        <h4>상품명</h4>
+        <p>상품설명</p>
+        </div>
+        
+        <div className="col-md-4">
+
+        <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%"></img>
+        <h4>상품명</h4>
+        <p>상품설명</p>
+        </div>
+      </div>
+      </div>
+      </div>
+       
   );
 }
 
